07453feee44deccae15b83269e1236a4ef9dae3c
Pavel Tolkach <pb.tolkach@outlook.com>
2022-10-27 14:56:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Circle
+    {
+        static public double GetLong(double r)
+        {
+            return r * 2 * Math.PI;
+        }
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public string GetConfirm(double r, double x, double y)
+        {
+            if (Math.Sqrt(x * x + y * y) <= r)
+            {
+                return "принадлежит кругу";
+            }
+            else
+            {
+                return "не принадлежит кругу";
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите величину радиуса и координаты точки (x,y)");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double longCircle = Circle.GetLong(r);
+            double square = Circle.GetSquare(r);
+            string conf = Circle.GetConfirm(r, x, y);
+
+            Console.WriteLine($"Длина окружности: {longCircle:f2}\nПлощадь круга: {square:f2}\nПринадлженость точки: {conf}");
+            Console.ReadLine();
+        }
+    }
+}
